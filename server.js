5e5446f926b859b03e5e5ee1f74eb8980f18309e
hernj07 <Hernandezj5026@gmail.com>
2022-11-08 17:57:05 -0600

    figuring out path issues

--- server.js
+++ server.js
@@ -1,22 +1,17 @@
 const express = require('express')
 const path = require('path')
-const app = express()
+const app = express();
 const {bots, playerRecord} = require('../data')
 const {shuffleArray} = require('../utils')
 
-app.use(express.json())
 
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-});
 
-app.get("/styles", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.css"));
-});
 
-app.get("/js", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.js"));
-});
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "./public")));
+app.use('/styles', express.static(path.join(__dirname, "./public/index.css")));
+app.use('/js', express.static(path.join(__dirname, "./public/index.js")));
 
 
 
